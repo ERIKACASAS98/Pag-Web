61a9f6b726dd870d4c6c59e67f10fa1ca7160783
camil <camil@CamiloPC.lan>
2025-07-30 19:02:52 -0500

    Ejercicio

---
+++ src/main/java/com/mydata/mavenproject1/Tienda.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mydata.mavenproject1;
+
+/**
+ *
+ * @author camil
+ */
+public class Tienda { private String nombre;
+    
+}
