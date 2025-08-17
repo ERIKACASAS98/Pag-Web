b780e66fb8e906603e6ee131811b9d430e793583
camil <camil@CamiloPC.lan>
2025-08-17 00:32:36 -0500

    EJEMPLO

---
+++ src/main/java/com/mydata/mavenproject1/clase.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mydata.mavenproject1;
+
+/**
+ *
+ * @author camil
+ */
+public class clase {
+    private String nombre;
+    
+}
